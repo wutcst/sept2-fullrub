35103c890c0dfc4d696b09c410e2579c96d806ea
f-xiu <2859767435@qq.com>
2023-06-24 12:24:27 +0800

    test(测试后端) 测试了后端登录等接口

---
+++ web_project/src/test/java/com/example/web_project/controller/MainControllerTest.java
@@ -0,0 +1,29 @@
+package com.example.web_project.controller;
+
+import com.example.web_project.entity.User;
+import com.example.web_project.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class MainControllerTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    void login() {
+        User user= userService.getById(1);
+        assertEquals("123456",user.getPassword());
+    }
+
+    @Test
+    void login2(){
+        User user= userService.getById(2);
+        assertEquals("admin123",user.getPassword());
+    }
+
+}
